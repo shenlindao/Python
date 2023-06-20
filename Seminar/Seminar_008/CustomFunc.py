bb741ad50ffe0cf949248c3ae7a0cd61779ca21f
Artem <shenlin@yandex.ru>
2023-06-20 10:15:46 +0300

    seminar_and_homework_08_2

--- Seminar/Seminar_008/CustomFunc.py
+++ Seminar/Seminar_008/CustomFunc.py
@@ -6,7 +6,7 @@ def show_contacts(file_name):
         data = file.readlines()
         for contact in data:
             print(contact, end='')
-        input('\nНажмите любую клавишу для продолжения')
+        input('\nНажмите Enter для продолжения')
 
 def add_contact(file_name):
     os.system('CLS')
@@ -16,7 +16,7 @@ def add_contact(file_name):
         res += input('Введите имя: ') + ' '
         res += input('Введите телефон: ')
         file.write('\n' + res)
-    input('Контакт успешно добавлен! Нажмите любую клавишу для возврата')
+    input('Контакт успешно добавлен! Нажмите Enter для возврата')
 
 def search_contact(file_name):
     os.system('CLS')
@@ -25,38 +25,59 @@ def search_contact(file_name):
         contacts = file.readlines()
         for contact in contacts:
             if target in contact:
+                print('')
                 print(contact)
                 break
         else:
+            print('')
             print('Такого контакта не существует')
-    input('Нажмите любую клавишу для продолжения')
+    input('Нажмите Enter для продолжения')
+
+def delete_contact(file_name):
+    os.system('CLS')
+    with open(file_name, 'r', encoding='utf-8') as file:
+        data = file.readlines()
+        for n, contact in enumerate(data, start=1):
+            print(n, '-', contact, end='')
+    print('')
+    delete_index = int(input("\nВведите номер строки для удаления: ")) - 1
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for contact, line in enumerate(data):
+            if contact not in [delete_index]:
+                file.write(line)
+
+def edit_contact(file_name):
+    os.system('CLS')
+    with open(file_name, 'r', encoding='utf-8') as file:
+        data = file.readlines()
+        for n, contact in enumerate(data, start=1):
+            print(n, '-', contact, end='')
+    edit_index = int(input("\nВведите номер строки для редактирования: ")) - 1
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for contact, line in enumerate(data):
+            if contact in [edit_index]:
+                res = ''
+                res += input('Введите фамилию: ') + ' '
+                res += input('Введите имя: ') + ' '
+                res += input('Введите телефон: ')
+                file.write(res + '\n')
+            else:
+                file.write(line)
+    input('\nНажмите Enter для возврата')
 
 def drawing():
     print('1 - Показать все контакты')
     print('2 - Добавить контакт')
     print('3 - Найти контакт')
-    print('4 - Удалить контакт')
-    print('5 - Выход')
-
-def delete_contact(file_name):
-    os.system('CLS')
-    with open(file_name, 'r', encoding='utf-8') as data:
-        contact = data.read()
-        print(contact)
-    print('')
-    delete_index = int(input("Введите номер строки для удаления: ")) - 1
-    lines = contact.split("\n")
-    delete_lines = lines[delete_index]
-    lines.pop(delete_index)
-    print(f"Удалена запись: {delete_lines}\n")
-    with open(file_name, 'w', encoding='utf-8') as file:
-        file.write('\n'.join(lines))
+    print('4 - Редактировать контакт')
+    print('5 - Удалить контакт')
+    print('6 - Выход')
 
 def main(file_name):
     while True:
         os.system('CLS')
         drawing()
-        user_choice = int(input('Введите номер от 1 до 4: '))
+        user_choice = int(input('Введите номер нужной операции от 1 до 6: '))
         if user_choice == 1:
             show_contacts(file_name)
         elif user_choice == 2:
@@ -64,7 +85,9 @@ def main(file_name):
         elif user_choice == 3:
             search_contact(file_name)
         elif user_choice == 4:
-            delete_contact(file_name)
+            edit_contact(file_name)
         elif user_choice == 5:
+            delete_contact(file_name)
+        elif user_choice == 6:
             print('Хорошего дня!')
             return
